4949214a8bdfa7a4546b9927e32235f1d89deea3
Захаров Павел <pzakharov83@gmail.com>
2022-02-23 12:42:23 +0300

    download books from tululu.org added folder

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-import requests
 import random
+from pathlib import Path
 
+import requests
 
 # url = 'http://tululu.org/txt.php?id=32168'
 
 url = 'http://tululu.org/txt.php?id='
+
+Path('books').mkdir(exist_ok=True)
+
+parent = Path('books')
 
 for _ in range(10):
     book_id = random.randint(10000, 50000)
     response = requests.get(url + str(book_id))
     response.raise_for_status()
 
-    filename = f'{str(book_id)}.txt'
+    filename = parent / f'{str(book_id)}.txt'
     with open(filename, 'w+b') as f:
         f.write(response.content)
