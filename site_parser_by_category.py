7fea0e1f00356373cab4cadb3305cd653d7805c8
Захаров Павел <pzakharov83@gmail.com>
2022-03-06 22:33:35 +0300

    split argparse logic to new function, remove open from argparse

--- site_parser_by_category.py
+++ site_parser_by_category.py
@@ -1,10 +1,9 @@
 import json
 import logging
 from pathlib import Path
-from shutil import move
 
 import requests
-from configargparse import ArgParser, FileType
+from configargparse import ArgParser
 
 from download_api import check_for_redirect, download_image, download_txt
 from tululu_parsing_api import parse_book_page, parse_category_page
@@ -13,15 +12,14 @@ FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logger = logging.getLogger(__name__)
 
 
-def main():
-    logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+def read_arguments():
     parser = ArgParser()
     parser.add(
         '--category', required=True, type=int, help='category id to download'
     )
     parser.add('--begin_page', type=int, default=1, help='Start page')
     parser.add('--end_page', type=int, help='End Page')
-    parser.add('--dest_folder', type=str, default='', help='Specify folder')
+    parser.add('--dest_folder', type=str, default='.', help='Specify folder')
     parser.add(
         '--skip_imgs',
         action='store_true',
@@ -36,12 +34,17 @@ def main():
     )
     parser.add(
         '--json_path',
-        type=FileType('w', encoding='utf-8'),
+        type=str,
         default='books_catalog.json',
-        help='File name to use for catalog'
+        help='Filename to use for catalog'
     )
 
-    options = parser.parse_args()
+    return parser.parse_args()
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+    options = read_arguments()
     with requests.Session() as session:
         books = parse_category_page(
             session, options.category, options.begin_page, options.end_page
@@ -55,22 +58,10 @@ def main():
                 book = parse_book_page(response.text, book_id, url)
                 txts = Path(options.dest_folder) / 'books'
                 if not options.skip_txt:
-                    if options.dest_folder:
-                        download_txt(session, book, txts)
-                        book['book_path'] = book['book_path'].replace(
-                            options.dest_folder, ''
-                        )
-                    else:
-                        download_txt(session, book)
+                    download_txt(session, book, txts)
                 imgs = Path(options.dest_folder) / 'images'
                 if not options.skip_imgs:
-                    if options.dest_folder:
-                        download_image(session, book, imgs)
-                        book['img_src'] = book['img_src'].replace(
-                            options.dest_folder, ''
-                        )
-                    else:
-                        download_image(session, book)
+                    download_image(session, book, imgs)
                 for value in (
                     'full_img_url', 'download_url', 'download_params',
                     'image_filename'
@@ -82,12 +73,9 @@ def main():
 
             logger.debug(f'book DICT: {book}')
 
-        json.dump(books_json, options.json_path, ensure_ascii=False)
-        if options.dest_folder:
-            current_name = options.json_path.name
-            options.json_path.close()
-            new_name = Path(options.dest_folder) / current_name
-            move(current_name, new_name)
+        json_filepath = Path(options.dest_folder) / options.json_path
+        with open(json_filepath, 'w', encoding='utf-8') as file_handle:
+            json.dump(books_json, file_handle, ensure_ascii=False)
 
 
 if __name__ == '__main__':
